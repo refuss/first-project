c4fe34aa82a09086d58eb225ac5882cc4fda13cb
refuss <66163292+refuss@users.noreply.github.com>
2020-07-21 17:50:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace pierwszyprojekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj szerokość: ");
+            string a = Console.ReadLine();
+            Console.Write("Podaj wysokość: ");
+            string b = Console.ReadLine();
+
+            int x = int.Parse(a);
+            int y = int.Parse(b);
+
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
